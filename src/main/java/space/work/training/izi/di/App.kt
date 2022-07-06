11121c8fad9ddf2ce3fdfa194769f9af1ca7fe4b
david.maric <daxy026@gmail.com>
2022-07-06 16:59:06 +0200

    app class info

--- src/main/java/space/work/training/izi/di/App.kt
+++ src/main/java/space/work/training/izi/di/App.kt
@@ -10,8 +10,9 @@ class App : Application() {
     //write option, maybe bcs of hilt, idea was get from firebase save in base than read from base,
     // now we need to save in room somehow
     //get in list from profiles and homeList, on 24hours home item we get to postfragment individual
-    //image size correction, adding links images videos in chat, add group chat functions
+    //adding links images videos in chat, add group chat functions
     //adding some representation to profiles that are hidden
     //add user can see who saw his post and who liked and disliked, nobody can see that on my profile except me
     //comments are only visible to 1to1 person
+    //mvvvm, coroutines, firebase, hilt, flow, room
 }
